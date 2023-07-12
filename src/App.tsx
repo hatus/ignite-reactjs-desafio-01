09e81c7e18fc9813b96b2f76762efc7b3a1e972b
Hatus Níwman <hatusn@gmail.com>
2023-07-11 23:59:25 -0300

    create structure html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import { Circle, PlusCircle, Rocket, Trash } from 'lucide-react';
+import styles from './App.module.css';
+
 function App() {
-	return <div>hello world</div>;
+  return (
+    <div>
+      <header className={styles.header}>
+        <Rocket />
+
+        <span>todo</span>
+      </header>
+
+      <main className={styles.wrapper}>
+        <form className={styles.todoForm}>
+          <input type="text" placeholder="Adicione uma nova tarefa" required />
+          <button type="submit">
+            Criar <PlusCircle />
+          </button>
+        </form>
+
+        <div className={styles.todoList}>
+          <div className={styles.todoListHeader}>
+            <p>Tarefas criadas</p>
+
+            <p>Concluídas</p>
+          </div>
+
+          <div className={styles.todoListContent}>
+            <div className={styles.task}>
+              <button className={styles.uncheckedButton} />
+
+              <p className="titleTask">
+                Integer urna interdum massa libero auctor neque turpis turpis
+                semper. Duis vel sed fames integer.
+              </p>
+
+              <button className={styles.trashButton}>
+                <Trash size={24} />
+              </button>
+            </div>
+            <div className={styles.task}>
+              <button className={styles.uncheckedButton} />
+            </div>
+            <div className={styles.task}>d</div>
+            <div className={styles.task}>d</div>
+            <div className={styles.task}>d</div>
+            <div className={styles.task}>d</div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
